a97b0efca17bd08cec29c33f290a69d11c181914
hzy <15162191629@163.com>
2019-03-16 17:48:21 +0800

    dao层html

--- hzy-api/src/main/java/com/hzy/api/controller/TestController.java
+++ hzy-api/src/main/java/com/hzy/api/controller/TestController.java
@@ -1,20 +1,23 @@
 package com.hzy.api.controller;
 
 import com.hzy.application.utils.DateUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
 
 
-    @GetMapping("/date")
+    @RequestMapping("/date")
     public String getDate(){
         Date curDate = DateUtils.getCurDate();
         return "当前时间是："+curDate;
+    }
+
+    @RequestMapping("index")
+    public String index(){
+        return "/user/index";
     }
 }
